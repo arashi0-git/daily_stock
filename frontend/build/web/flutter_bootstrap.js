25f15f4bd41392bc91ebeebd5a95fe4177b993fb
arashi0-git <Y.arashi0408@icloud.com>
2025-07-01 11:34:37 +0900

    add

--- frontend/build/web/flutter_bootstrap.js
+++ frontend/build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "966345499"
+    serviceWorkerVersion: "2078133337"
   }
 });
